4d73841ec5aabe2d2ddcb56eadbff21581d12aee
Jeff Derbes <derbes.jeffrey@gmail.com>
2022-09-13 10:53:15 -0500

    style fixes

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -3,7 +3,7 @@ import logo from '../../assets/hero-image.jpeg';
 
 export function Hero() {
   return (
-    <div className="mt-[-25px] md:mt-[-125px]">
+    <div className="mt-[-25px] md:mt-[-100px] lg:mt-[-200px]">
       <Image src={logo} width={1920} height={500} className="max-h-full" />
     </div>
   );
